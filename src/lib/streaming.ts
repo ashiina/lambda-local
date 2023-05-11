4acb1772079f234036dc20dae339191936d79d8c
Simon Knott <info@simonknott.de>
2023-05-11 10:01:20 +0200

    add docstring

--- src/lib/streaming.ts
+++ src/lib/streaming.ts
@@ -1,3 +1,12 @@
+/**
+ * Implements Lambda Response Streaming by polyfilling
+ * `awslambda.streamifyResponse` and `awslambda.HttpResponseStream.from`.
+ * 
+ * If they're used, `execute` will return a `ReadableStream` as `body`.
+ * 
+ * See https://aws.amazon.com/fr/blogs/compute/introducing-aws-lambda-response-streaming/ for reference.
+ */
+
 import { PassThrough } from "stream";
 
 function streamifyResponse(handler) {
